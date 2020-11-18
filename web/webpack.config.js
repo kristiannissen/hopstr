9fb001cd53932579ab15d45273dfac32a1b45537
Kristian Nissen <kristian.nissen@gmail.com>
2020-11-18 23:20:06 +0100

    Folder structure

---
+++ web/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "assets/js/"),
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/react"],
+            plugins: ["@babel/plugin-transform-runtime"],
+          }
+        }
+      }
+    ]
+  }
+};
