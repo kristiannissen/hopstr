a8450d80ea380e69483087b3f92bf93b30caba39
Kristian Nissen <kristian.nissen@gmail.com>
2020-11-26 08:24:42 +0100

    A new beginning

--- main.go
+++ main.go
@@ -1,34 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
-    "fmt"
-    "regexp"
 )
 
 func main() {
 
-    port := os.Getenv("PORT")
+	port := os.Getenv("PORT")
 	if port == "" {
 		port = "80"
 		log.Printf("Default port %s", port)
 	}
 
-    routes := make(map[string]string)
-    routes["^\\/$"] = "hello kitty"
-    routes["\\/hops\\/$"] = "hello pussy"
-    routes["\\/hops/(\\w+)\\/$"] = "hello kitty pussy"
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		var path string = r.URL.Path
 
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        var path string = r.URL.Path
-        for k, v := range routes {
-            m, err := regexp.MatchString(k, path)
-            fmt.Println(m, err, v)
-        }
-        fmt.Fprint(w, "Done "+ path)
-    })
+		fmt.Fprint(w, "Done "+path)
+	})
 
 	log.Printf("Listening on port %s", port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
