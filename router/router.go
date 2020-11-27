37c66633f45e837253b04bcc5f59c87a29051c3e
kristiannissen <kn@unisport.dk>
2020-11-27 18:31:13 +0100

    gofmt

--- router/router.go
+++ router/router.go
@@ -1,48 +1,50 @@
 package router
 
 import (
-  "regexp"
-  str "strings"
+	"regexp"
+	str "strings"
 )
+
 // Router
 type Router struct {
-  Routes map[string]string
-  Params map[string]string
+	Routes map[string]string
+	Params map[string]string
 }
+
 // Init function
 func init() {
-  // fmt.Println("Init called")
+	// fmt.Println("Init called")
 }
+
 // Used only to test
 func Hello(x int) int {
-  return x * 2
+	return x * 2
 }
+
 // Creates a new Router
 func NewRouter() *Router {
-  // FIXME: On new the Routes needs to be empty
-  return &Router{Routes: make(map[string]string)}
+	// FIXME: On new the Routes needs to be empty
+	return &Router{Routes: make(map[string]string)}
 }
+
 // Adds a new route to routes
 func (r *Router) AddRoute(path string, s string) {
-  var p string = rewrite(path)
+	var p string = rewrite(path)
 
-  r.Routes[p] = s
+	r.Routes[p] = s
 }
+
 // Rewrite string to regexp
 func rewrite(path string) string {
-  reg := regexp.MustCompile(`\{(?P<key>[a-z]+)\}`)
-  groups := reg.FindStringSubmatch(path)
+	reg := regexp.MustCompile(`\{(?P<key>[a-z]+)\}`)
+	groups := reg.FindStringSubmatch(path)
 
-  if len(groups) > 0 {
-    // fmt.Println("rep is ", groups[0], path)
-    // TODO: Add the key to the params map
-    // Replace key with regexp string
-    path = str.ReplaceAll(path, groups[0], "([a-zA-Z0-9]+)")
-  }
-  path = str.ReplaceAll(path, "/", "\\/")
-  return path
-}
-// Dispatch route to handler
-func Dispatch() string {
-  return "Hello"
+	if len(groups) > 0 {
+		// fmt.Println("rep is ", groups[0], path)
+		// TODO: Add the key to the params map
+		// Replace key with regexp string
+		path = str.ReplaceAll(path, groups[0], "([a-zA-Z0-9]+)")
+	}
+	path = str.ReplaceAll(path, "/", "\\/")
+	return path
 }
--- router/router_test.go
+++ router/router_test.go
@@ -1,42 +1,42 @@
 package router
 
 import (
-  "testing"
-  "fmt"
-  r "router"
+	"fmt"
+	r "router"
+	"testing"
 )
 
 func TestHello(t *testing.T) {
-  x := r.Hello(2)
-  fmt.Println("**")
+	x := r.Hello(2)
+	fmt.Println("**")
 
-  if x != 4 {
-    t.Error("Expected 2 * 2 to equal 4")
-  }
+	if x != 4 {
+		t.Error("Expected 2 * 2 to equal 4")
+	}
 }
 
 func TestNewRoute(t *testing.T) {
-  route := r.NewRouter()
+	route := r.NewRouter()
 
-  if len(route.Routes) > 0 {
-    t.Errorf("New is not returning empty map")
-  }
+	if len(route.Routes) > 0 {
+		t.Errorf("New is not returning empty map")
+	}
 }
 
 func TestAddRoute(t *testing.T) {
-  route := r.NewRouter()
-  route.AddRoute("kitty", "Kitty")
-  route.AddRoute("pussy", "Pussy")
+	route := r.NewRouter()
+	route.AddRoute("kitty", "Kitty")
+	route.AddRoute("pussy", "Pussy")
 
-  if len(route.Routes) != 2 {
-    t.Error("AddRoute did not add any routes")
-  }
+	if len(route.Routes) != 2 {
+		t.Error("AddRoute did not add any routes")
+	}
 }
 
 func TestAddRouteRewrite(t *testing.T) {
-  route := r.NewRouter()
-  route.AddRoute("/hello/{pussy}/", "hello")
-  route.AddRoute("/hello/{kitty}/hello/", "hello")
+	route := r.NewRouter()
+	route.AddRoute("/hello/{pussy}/", "hello")
+	route.AddRoute("/hello/{kitty}/hello/", "hello")
 
-  t.Error("shit")
+	t.Error("shit")
 }
