ed2e7c49a16f8f9725cae76d7cfa3ec51c6088b9
kristiannissen <kn@unisport.dk>
2020-11-27 14:39:38 +0100

    Working on FindStringSubmatch

--- router/router.go
+++ router/router.go
@@ -1,15 +1,35 @@
 package router
 
+import (
+  "regexp"
+  "fmt"
+)
 // Router
 type Router struct {
-  routes map[string]string
+  Routes map[string]string
 }
 // Used only to test
 func Hello(x int) int {
   return x * 2
 }
+// Creates a new Router
+func NewRouter() *Router {
+  return &Router{Routes: make(map[string]string)}
+}
+// Adds a new route to routes
+func (r *Router) AddRoute(path string, s string) {
+  var p string = rewrite(path)
 
-func (r *Router) AddRoute(path string, s string) *Router {
-  r.routes[path] = s
-  return r
+  r.Routes[p] = s
+}
+// Rewrite string to regexp
+func rewrite(path string) string {
+  reg := regexp.MustCompile(`\{(?P<key>[a-z]+)\}`)
+  groups := reg.FindStringSubmatch(path)
+  fmt.Println(groups[0:])
+  return path
+}
+// Dispatch route to handler
+func Dispatch() string {
+  return "Hello"
 }
--- router/router_test.go
+++ router/router_test.go
@@ -8,8 +8,35 @@ import (
 
 func TestHello(t *testing.T) {
   x := r.Hello(2)
-  fmt.Print(x)
+  fmt.Println("Hello Kitty")
+
   if x != 4 {
     t.Error("Expected 2 * 2 to equal 4")
   }
+}
+
+func TestNewRoute(t *testing.T) {
+  route := r.NewRouter()
+
+  if len(route.Routes) > 0 {
+    t.Errorf("New is not returning empty map")
+  }
+}
+
+func TestAddRoute(t *testing.T) {
+  route := r.NewRouter()
+  route.AddRoute("kitty", "Kitty")
+  route.AddRoute("pussy", "Pussy")
+
+  if len(route.Routes) != 2 {
+    t.Error("AddRoute did not add any routes")
+  }
+}
+
+func TestAddRouteRewrite(t *testing.T) {
+  route := r.NewRouter()
+  route.AddRoute("/hello/{pussy}/", "hello")
+  route.AddRoute("/{kitty}/hello/{pussy}/", "hello")
+
+  t.Error("shit")
 }
