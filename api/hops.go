b4ce5d2f1e26b10eb2e74e4391b59d6535b4132c
kristiannissen <kristian.nissen@gmail.com>
2024-07-03 22:24:17 +0200

    httptest added

--- api/hops.go
+++ api/hops.go
@@ -1,11 +1,36 @@
 package api
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
+
+	"hopstr/api/_pkg/domain"
 )
 
 func Hops(w http.ResponseWriter, req *http.Request) {
-	w.WriteHeader(http.StatusOK)
+	//
+	w.Header().Set("Content-type", "application/json; charset=utf-8")
 
-	w.Write([]byte("Have some hops"))
+	repo := domain.NewVercelHopRepository()
+	hoplist := &domain.Hoplist{}
+	var err error
+
+	if hoplist, err = repo.List(); err != nil {
+		log.Println(err)
+
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("404"))
+	} else {
+		var b []byte
+		if b, err = json.MarshalIndent(hoplist, "", "  "); err != nil {
+			log.Println(err)
+
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("500 - internal server error"))
+		} else {
+			w.WriteHeader(http.StatusOK)
+			w.Write(b)
+		}
+	}
 }
--- api/hops_test.go
+++ api/hops_test.go
@@ -1,10 +1,13 @@
 package api
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"hopstr/api/_pkg/domain"
 )
 
 func TestHops(t *testing.T) {
@@ -17,6 +20,13 @@ func TestHops(t *testing.T) {
 	defer res.Body.Close()
 
 	resp, _ := io.ReadAll(res.Body)
+	hoplist := &domain.Hoplist{}
 
-	t.Error(string(resp))
+	if err := json.Unmarshal(resp, &hoplist); err != nil {
+		t.Error(err)
+	} else {
+		if len(hoplist.Hoplist) == 0 {
+			t.Error("List is empty")
+		}
+	}
 }
