0f25336526bf32946e4fcbc125d458c512a4f72f
kristiannissen <kristian.nissen@gmail.com>
2024-07-17 13:16:52 +0200

    Test case

--- api/hello.go
+++ api/hello.go
@@ -5,13 +5,5 @@ import "net/http"
 func Hello(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(http.StatusOK)
 
-	resp := make(chan []byte)
-
-	go func() {
-		resp <- []byte("Hello Pussy")
-	}()
-
-	response := <-resp
-
-	w.Write(response)
+	w.Write([]byte("Hello Kitty"))
 }
